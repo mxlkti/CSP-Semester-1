883249e6e79b94f70bc29303e1205d026d3bc85b
mxlkti <tglau@cps.edu>
2025-01-13 08:27:38 -0600

    Add files via upload

---
+++ pokemon.py
@@ -0,0 +1,320 @@
+#Titus Lau
+#11/20/24
+#Period 5
+
+#init
+level=0
+petname="Mysterious Pokeball"
+name="Titus obviously"
+day=0
+import random
+import math
+save_file = "pokemon_game_save.txt"
+
+#functions
+def save_game():
+    with open(save_file, "w") as file:  # Open the save file in write mode
+        file.write(petname + "\n")
+        file.write(name + "\n")
+        file.write(str(level) + "\n")
+        file.write(str(day) + "\n")
+    print("Game saved successfully!")
+
+def load_game():
+    global petname, level, day, name
+    try:
+        with open(save_file, "r") as file:
+            petname = file.readline().strip()
+            name = file.readline().strip()
+            level = int(file.readline().strip())
+            day = int(file.readline().strip())
+        print("Game loaded successfully!")
+    except FileNotFoundError:
+        print("No save file found. Start a new game!")
+    except ValueError:
+        print("Save file is corrupted. Start a new game!")
+
+def ball():
+    print("""
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣤⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣾⣿⣿⡿⠿⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠿⢿⣿⣿⣷⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⢀⣴⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣦⡀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⣠⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣄⠀⠀⠀⠀
+      ⠀⠀⠀⣰⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣆⠀⠀⠀
+      ⠀⠀⣸⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣆⠀⠀
+      ⠀⢰⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⡆⠀
+      ⢀⣾⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⠀
+      ⢸⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⡇
+      ⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣷
+      ⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿
+      ⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿
+      ⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿
+      ⢻⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⡇
+      ⢸⣿⣿⣿⣿⣿⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣶⣿⣿⣿⣿⠃
+      ⠀⢻⣿⣿⡈⠻⢿⣿⣿⣷⣤⣄⣀⠀⠀⠀⠀⠀⢀⣴⣿⣿⡿⠛⠉⢀⣀⣀⡀⠉⠻⢿⣿⣿⣆⠀⠀⠀⠀⠀⠀⣀⣠⣴⣾⣿⣿⡿⠟⣿
+      ⠀⠀⢿⣿⣷⠀⠀⠈⠛⠿⢿⣿⣿⣿⣷⣶⣤⣤⣾⣿⡿⠋⠀⣴⠞⠉⠁⠈⠙⠻⣆⠀⠹⣿⣿⣧⣤⣤⣶⣾⣿⣿⣿⡿⠟⠛⣿⣿⣾⠁⠀
+      ⠀⠀⠈⢿⣿⣷⡀⠀⠀⠀⠀⠀⠉⠙⠛⠿⠿⣿⣿⣿⡇⠀⢸⡃⠀⠀⠀⠀⠀⠀⢹⡇⠀⢻⣿⣿⣿⠿⠿⠟⠋⠉⠀⠀⠀⠀⢀⣾⣿⡿⠀⠀
+      ⠀⠀⠀⠈⢿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣧⠀⠸⣧⡀⠀⠀⠀⠀⢀⡾⠃⢀⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⡿⠁⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠻⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣷⣄⠈⠛⠶⠦⠴⠶⠋⠁⣠⣾⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⠏⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣶⣀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣶⣦⣤⣤⣶⣾⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⡟⠁⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠘⠻⣿⣿⣷⣤⡀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠻⠿⠿⠿⠿⠛⠛⠉⠀⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⢿⣿⣿⣷⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣿⡿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⢿⣿⣿⣿⣶⣶⣦⣤⣤⣤⣤⣤⣤⣤⣤⣴⣶⣾⣿⣿⣿⡿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠙⠛⠻⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠟⠛⠋⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+          """)
+
+def eevee():
+    print("""
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⡤⠴⠚⠋⣉⣠⣤⠆⠲⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠰⠋⠁⣠⣴⣾⣿⣿⡿⠃⣨⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠉⠀⣤⡴⣟⣿⣳⣿⣾⡿⠁⣰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⣧⠈⣉⠉⠁⠒⠂⠠⠄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠋⠀⣤⣛⢧⣻⣽⡿⣯⣷⡿⠁⣰⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠘⢧⠀⠄⠈⠀⠒⠠⠄⡀⠈⠑⠢⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠤⣀⠀⠀⠀⠀⠀⣰⠃⠀⣾⣵⣾⣿⣿⣿⣿⣿⡟⢁⡴⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠈⢳⡀⠢⠀⠀⠀⢄⠀⠉⠰⣤⣀⠹⢤⣄⠀⠀⠀⣀⠀⢺⡅⠀⠐⠚⠒⠾⡄⠈⢳⣶⢦⣄⣰⠇⢀⣾⣿⣿⣿⣿⣿⣿⣿⠟⣠⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡟⢦⠀⠀
+      ⠀⠀⠀⠱⣄⠑⣄⢀⠠⣑⢮⣳⣼⣿⣷⣆⡈⢣⡀⢘⡏⠉⠘⠛⢦⠀⠀⠀⠀⠀⠀⠀⠁⠈⢻⠏⢀⣾⣿⣿⣿⣿⣿⣿⠟⣣⣞⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡟⠘⠈⣥⠀
+      ⠀⠀⠀⠀⠈⠳⣌⠻⢷⣿⣾⣿⣿⣿⣿⣿⣿⡄⣞⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠚⠁⠘⠋⣼⣿⣿⠿⢋⣱⡼⠙⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠎⠀⠀⠀⢸⠄
+      ⠀⠀⠀⠀⠀⠀⠈⠳⢦⣉⠛⠿⢿⣿⣿⣿⣿⣟⡲⡕⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣰⣶⣩⡶⠾⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⡤⠴⠒⠋⠀⠀⠀⠁⠀⠀⡆
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠳⠶⢤⣬⣉⣛⠻⣓⠅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡍⢻⡆⠀⠀⠀⠀⠀⠀⠀⣠⠤⠖⠛⣈⠉⡐⢂⡜⡁⠀⠀⠀⠀⠀⠀⠀⢱
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⡻⠀⠈⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠴⢂⣤⡀⠀⠀⠘⢸⡇⠀⠀⠀⠀⢀⡴⠋⢅⠢⠑⡨⠀⠀⡑⢢⣊⡀⠄⡖⠀⠀⠀⠀⠀⢸
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⠀⠀⣤⠞⠇⠀⠀⠀⠀⠀⠀⠀⢠⣿⡏⢀⣆⠀⠀⠀⡏⠀⠀⢀⡴⢫⠔⡉⢄⠢⠑⡄⢃⠔⡠⢉⠡⡐⠌⣂⠠⡀⠀⠀⠀⢸
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⠀⢰⣿⣤⣾⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣾⣧⠀⠀⠀⡇⠀⢀⡾⣙⢆⡣⢌⠰⢈⠡⡐⠌⡰⢀⠣⡐⠄⡃⢄⠂⠥⢀⠠⣚⡘
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡆⢸⣿⣿⣿⠀⠀⢀⣶⠀⠀⠀⠀⢿⢿⣿⡇⠀⢀⣰⡏⠴⣸⡏⡸⡎⡶⢁⠆⡁⢆⠁⡎⠠⡈⢠⠀⢇⡈⢠⠈⡄⢁⠴⢁⡇
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣧⠈⠶⠿⠟⠀⠀⠀⠁⠀⠀⠀⡀⠈⠉⠉⠀⡄⠎⡼⠀⠀⠉⠻⣵⢣⡝⣣⠎⡰⠈⡔⠠⢃⠜⡠⢉⠤⡘⠄⢣⠘⠤⣉⠆⡇
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣏⠀⠀⠀⠀⠀⠓⠒⠉⠃⠒⠋⠁⢀⢠⠒⣍⠸⣸⠃⠐⠂⠤⢀⢹⣣⢞⡱⣎⠥⣃⠔⡡⢊⠤⠑⡌⠰⢠⠉⢆⡘⢒⠤⢺⠄
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⣄⡀⡀⣀⢀⡀⣀⠠⢄⡐⠦⡑⣊⢱⡤⠟⠁⠀⠀⠀⠀⠀⠙⢶⢫⢶⣍⡞⣜⢢⡕⢢⢌⡑⣨⢑⢢⡉⢦⡘⣅⢪⡟⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⡁⠉⠒⡶⢬⣰⣁⣞⣠⣃⠶⠵⠚⠉⢣⠀⠀⠀⠀⠀⠀⠀⡀⢀⢣⢊⠹⢾⣥⡳⢞⡱⣎⡳⣥⢋⡦⣙⢦⡱⣬⠟⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠖⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢣⠀⠀⠀⠀⠀⠀⢡⢸⢸⢠⠃⠎⢷⡻⣭⠷⣭⢳⣎⠷⡜⢵⣪⠞⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣐⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡌⡜⢢⠉⡜⠸⣿⣾⣽⣎⡷⣮⠟⠞⠉⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣉⠀⠀⠀⠀⡌⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡞⠟⡘⢄⠣⢌⡑⣾⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⡄⠀⠀⠀⢃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⢀⣎⣠⢔⢣⣼⠎⡜⣠⠋⡴⣈⠽⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠢⣀⠀⠀⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⠴⢋⠔⣠⢪⠿⣆⢣⠒⡤⢓⠴⣡⠖⠹⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠒⠀⠷⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⠋⠀⠁⠳⡌⣴⡏⠀⠉⠳⣭⣒⣭⠶⠏⡌⠡⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢶⣤⠀⢀⠀⠀⠀⢠⠁⠀⠀⠀⠀⢰⡿⠁⠀⠀⠀⠀⢸⡏⣴⠡⣮⢁⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⢷⣾⡀⢰⣶⠆⠀⠀⠀⠀⠀⣘⠃⠀⠀⠀⠀⠀⠈⠳⣧⣘⣳⡼⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣍⡞⣻⣟⡼⠁⠄⣀⠀⠀⢠⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠓⠿⣷⠋⡝⠀⣂⡁⠛⣠⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣞⣀⣸⠅⣈⣴⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+          """)
+
+def leafeon():
+    print("""
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣄⠀⠀⠀⠀⠀⠀⢀⡴⠒⠋⠉⠉⣉⣁⠤⠖⢢⠟⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⢀⡠⠔⣻⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠋⣸⡀⠀⠀⠀⠀⢠⠋⠀⠀⢀⡴⠋⠁⠀⢀⡴⠃⠀⠀⠀
+      ⠀⠀⠀⠀⠀⢀⢔⡿⠂⢰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠁⢠⠳⡆⠀⠀⠀⢠⡃⠀⠀⠀⡜⠀⠀⠀⢠⡏⠀⠀⠀⠀⠀
+      ⠀⠀⠀⢀⠞⢡⠎⠀⠀⣯⡴⠒⠉⠉⠉⠑⠢⣄⠀⠀⠀⠀⠀⠀⠀⡴⠊⠀⠀⣸⠀⡇⠀⠀⣰⠃⠸⡀⠀⡸⠁⠀⠀⠀⢸⠃⠀⠀⠀⠀⠀
+      ⠀⠀⢠⠋⢀⡏⠀⢀⣴⠋⣀⡀⠀⠀⠀⠀⠀⠈⠳⡀⠀⠀⠀⡠⠒⠻⠄⠀⢠⣇⡀⡇⠀⢠⠇⠀⠀⠱⣀⠃⠀⠀⠀⠀⡞⠀⠀⠀⠀⠀⠀
+      ⠀⢀⡷⠀⡼⡇⠀⠀⠀⡎⠉⠀⢭⣷⡀⠀⠀⠀⠀⣳⠀⣠⠎⠀⠀⠀⢀⣴⠏⠁⠀⠇⠀⢸⠀⠀⠀⠀⢻⠀⠀⠀⠀⠨⢇⠀⠀⠀⠀⠀⠀
+      ⠀⡼⠀⢰⠃⢳⠀⠀⠰⡃⠀⠀⡼⢸⡅⠀⠀⠀⢀⡎⡴⠁⠀⠀⣠⠖⠉⡞⠀⢠⣼⠀⠀⠘⡄⠀⠀⠀⠸⡄⠀⠀⢀⡴⠉⠙⢦⡀⠀⠀⠀
+      ⠀⣧⠀⡎⡆⢸⢀⣀⣼⡿⣄⡞⢀⠞⠁⠀⠀⣀⣾⡼⠥⢄⣠⠞⠁⠀⣸⠁⠀⢠⠏⠀⠀⠀⠘⢆⠀⠀⠀⠘⢦⡰⠋⠀⠀⠀⠀⠙⣄⠀⠀
+      ⢠⣯⠀⡇⠀⢸⠀⢀⡟⠀⢘⡴⠁⠀⣀⠴⠊⠉⢿⡇⠀⢠⠏⠀⠀⢠⠇⠀⢀⡎⠀⠀⠀⠀⠀⠈⠣⡀⠀⠀⠀⠱⡄⠀⠀⠀⠀⠀⠸⡄⠀
+      ⠀⢻⠀⢳⡀⢸⠐⡞⠀⠀⡜⠀⢠⠊⠁⠀⠀⠀⢸⠀⢁⡏⠀⢀⡴⠛⣶⣦⠞⠀⠀⠀⠀⠀⠀⠀⠀⠱⡀⠀⠀⠀⠹⡀⠀⠀⠀⠀⠀⣇⠀
+      ⠀⠀⢣⡀⠳⣼⠀⠀⠀⠀⢧⢠⠃⠀⠀⠀⠀⠀⠀⠀⣼⡠⠖⢫⢀⣠⠼⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢩⠷⠚⠂⠀⢳⠀⠀⠀⠀⠀⢹⠀
+      ⠀⠀⠀⠙⣤⡘⠀⠀⠀⠀⠈⢯⠀⠀⠀⠀⠀⠀⠀⠀⠋⢀⣤⡔⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠷⣄⣄⠀⠈⣇⠀⠀⠀⠀⣘⡆
+      ⠀⠀⠀⠘⠣⣄⠀⣀⡀⠀⠀⠀⠀⠀⠀⣀⣀⡀⠀⠀⠀⣠⢴⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣀⣀⡀⣸⠀⠀⠀⣨⠇⠀
+      ⠀⠀⠀⠀⠀⠈⢣⣿⣟⣖⠀⠀⠀⠰⣸⣙⡆⣹⠃⠀⡼⢁⠜⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡰⠋⠀⠀⣠⠟⠉⠓⠢⠻⣅⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⢏⠻⠿⠆⠀⠀⠀⠟⠲⠖⠋⠀⣴⢛⣡⡴⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⠼⠥⢄⣠⠞⠁⠀⠀⠀⢀⡴⠚⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠘⠢⣀⠀⠐⠂⠀⠀⠀⠀⠀⠀⠛⠻⠭⣀⣀⠤⠖⠒⠉⠉⠁⠀⠉⠑⠢⣰⠋⠀⠠⠔⠊⠉⠙⠒⣀⡤⠖⠋⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⢓⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⢶⠦⠤⠤⠤⠖⠚⠉⠁⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡤⠼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⢘⡲⠐⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⢏⠉⠉⠁⠀⢀⡼⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠈⠓⠤⠤⠤⣨⡴⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⠀⠀⠀⠀⠀⠀⠸⡅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠱⡀⠀⠀⠀⠀⠀⠀⡤⠴⠒⠛⠢⣀⠀⠀⠈⠢⣀⠀⠀⠀⠀⠀⠙⢆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢱⡀⠀⠀⠀⠀⡼⠁⠀⠀⠀⠀⠀⣹⠆⠀⠀⣸⠗⠢⣄⠀⠀⠀⡼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⡠⠞⡀⠀⠀⠀⡼⠀⠀⠀⠀⠀⠀⢠⠃⠀⠀⢠⡏⠀⠀⡇⠀⢀⡰⢧⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠶⣶⠏⠀⠀⢰⠸⣇⠀⠀⠀⠀⠀⣠⣷⣤⣀⣰⡟⠀⠀⢀⡷⠀⠀⢱⢤⡴⠽⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣏⠀⠀⠀⣬⣷⣜⣦⡀⠀⢰⣿⡿⣿⣿⣿⠟⠀⠀⠀⣼⠟⠒⢲⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡾⠉⠙⢲⣾⣿⣿⠃⠈⠁⠀⠘⠿⠿⠿⠟⠁⠀⠀⠀⣿⡏⠀⢀⣼⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡟⠁⠀⢀⣾⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠛⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡛⠓⠺⡟⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+          """)
+
+def evolve1():
+      if level==10:
+            global petname
+            print(" ")
+            eevee()
+            print("WOOOOSH")
+            print("Your Pokemon has evolved from a Pokeball to an Eevee!")
+            name_change=input("Would you like to rename your pet?")
+            if name_change=="yes" or name_change=="y":
+                  petname=input("What would you like to name your Eevee?")
+            else:
+                  print("Level up your Pokemon to reach new evolutions!")
+
+def evolve2():
+      if level==20:
+            global petname
+            print(" ")
+            leafeon()
+            print("WOOOOSH")
+            print("Your Pokemon has evolved from an Eevee to an Leafeon!")
+            name_change=input("Would you like to rename your pet?")
+            if name_change=="yes" or name_change=="y":
+                  petname=input("What would you like to name your Leafeon?")
+            else:
+                  print("Level up your Pokemon to reach new evolutions!")
+            print(" ")
+      else:
+            print(" ")
+
+def menu():
+    print("Welcome to My Pocket Pokemon!") #start
+    print("You are a famous Pokemon trainer from a far away land.")
+    print("Your task is to train a mysterious Pokeball found in the corners of the world.")
+    name=input("What is your name?")
+    print(" ")
+    print("Welcome "+name+"!")
+    petname=input("What is your Pokeball's name?")
+    ball()
+    print("Great! "+petname+" is ready to start training!")
+    print(" ")
+    print("Welcome to your gym: "+name+" Center Gym.")
+    print("Here you can check the stats, train, and battle other Pokemon.")
+    print(" ")
+    print("To exit the game, enter 'stop'")
+    print("To save the game, enter 'save'")
+    print("To load a saved game, enter 'load'")
+    print(" ")
+
+    while True:
+      global day
+      global level
+      day=day+1
+      evolve1()
+      evolve2()
+      print("Day: "+str(day))
+      print("What would you like to do today?")
+      activity=input("You can train, check stats, or battle other Pokemon.")
+      if activity=="train" or activity=="tra" or activity=="t":
+            print(" ")
+            print("In the training center we can feed, bathe, and spar with our Pokemon.")
+            train_act=input("What would you like to do today?")
+            if train_act=="feed" or train_act=="f":
+                  print(" ")
+                  print(petname+" was hungry and you fed it a berry.")
+                  print(petname+" is well-fed.")
+                  print("  Pokemon level increased by 10XP")
+                  if level<10:
+                       level=level+1
+                  elif 10<=level<20:
+                       level=level+0.5
+                  elif 20<=level:
+                       level=level+0.2
+            elif train_act=="bathe" or train_act=="b":
+                  print(" ")
+                  print(petname+" was dirty and you gave it a bath.")
+                  print(petname+" is squeaky-clean.")
+                  print("  Pokemon level increased by 10XP")
+                  if level<10:
+                       level=level+1
+                  elif 10<=level<20:
+                       level=level+0.5
+                  elif 20<=level:
+                       level=level+0.2
+            elif train_act=="spar" or train_act=="s":
+                  print(" ")
+                  print(petname+" trained all day with you in the gym.")
+                  print(petname+" is getting stronger.")
+                  print("  Pokemon level increased by 10XP")
+                  if level<10:
+                       level=level+1
+                  elif 10<=level<20:
+                       level=level+0.5
+                  elif 20<=level:
+                       level=level+0.2
+            else:
+                  print(" ")
+                  print("I didn't get that, try typing 'feed' or 'spar'.")
+                  print(petname+" decided to rest for today and took a nap.")
+
+      elif activity=="stats" or activity=="check" or activity=="check stats" or activity=="stat":
+            if level>10:
+                  ball()
+            elif 10>=level>20:
+                  eevee()
+            else:
+                  leafeon()
+            print("Your Pokemon's current level is "+str(math.floor(level))+".")
+            if level<20:
+                 print("Level up your Pokemon to reach new evolutions!")
+            print(petname+" played in the gym while you researched their level.")
+
+      elif activity=="battle" or activity=="bat" or activity=="b":
+            if level<3:
+                  print("Please try leveling up your pet to level 3 to battle.")
+                  print("Your Pokemon, looking defeated, did some pushups in protest.")
+            else:
+                  print("Welcome to the Arena Gym.")
+                  print("Here you can battle other Pokemon.")
+                  print(" ")
+                  print("A new challenger has arrived!")
+                  opp=random.randint(1,3)
+                  if opp==1:
+                        opp_name="Pikachu"
+                  elif opp==2:
+                        opp_name="Bulbasaur"
+                  elif opp==3:
+                        opp_name="Squirtle"
+                  print("Let's welcome "+opp_name+" to the ring!")
+                  for i in range(3):
+                        dialouge=random.randint(1,7)
+                        if dialouge==1:
+                             print(petname+" pulled "+opp_name+"'s hair, looks like they were wearing a wig.")
+                        elif dialouge==2:
+                             print(opp_name+"came up from behind and jumped "+petname+".")
+                        elif dialouge==3:
+                             print(petname+" did a flip. 0 damage but looked cool.")
+                        elif dialouge==4:
+                             print(petname+" ran over "+opp_name+" with their Tesla Cybertruck. Yikes.")
+                        elif dialouge==5:
+                             print(opp_name+" has gun. RUN!")
+                        elif dialouge==6:
+                             print(petname+"? Where did you get a bomb-")
+                        elif dialouge==7:
+                             print(opp_name+" set the stadium on fire, everybody stop, drop, and roll!")
+                  print(" ")
+                  win=random.randint(1,2)
+                  if win==1:
+                        print(petname+" has won the gym battle!")
+                        print("Congratulations!")
+                        print("  Pokemon level increased by 20XP")
+                        if level==9:
+                             level=level+1
+                        elif level<10:
+                              level=level+2
+                        elif 10<=level<20:
+                              level=level+1
+                        elif 20<=level:
+                              level=level+0.4
+                  if win==2:
+                        print(opp_name+" has won the gym battle!")
+                        print(petname+" still had a fun time.")
+                        print("  Pokemon level increased by 10XP")
+                        if level<10:
+                              level=level+1
+                        elif 10<=level<20:
+                              level=level+0.5
+                        elif 20<=level:
+                              level=level+0.2
+
+      elif activity=="stop":
+            print(" ")
+            print("Thank you for playing! We hope to see you again!")
+            break
+      elif activity=="save":
+            save_game()
+      elif activity=="load":
+            load_game()
+      else:
+            print(" ")
+            print("I didn't get that, try typing stats, train, or battle.")
+            print(petname+" decided to rest for today and took a nap.")
+
+#main
+menu()
---
+++ ticket.py
@@ -0,0 +1,135 @@
+#Titus Lau
+#Period 5
+#12/11/24
+
+#init
+import turtle
+t=turtle.Turtle()
+
+#function    
+def ticket_quiz():
+    
+    def draw_ticket(name, price, dayofweek, y_location):
+        t.goto(-50, y_location)
+        t.write("Ticket", font=("Arial", 15), align="right")
+        t.pendown()
+        for i in range(2):
+            t.forward(500)
+            t.left(90)
+            t.forward(250)
+            t.left(90)
+        t.penup()
+        t.goto(50, y_location +215)
+        t.write("Admit One", font=("Arial", 15), align="right")
+        t.goto(440, y_location +215)
+        t.write(dayofweek, font=("Arial", 15), align="right")
+        t.goto(200, y_location +115)
+        t.write(name, font=("Arial", 20), align="center")
+        t.goto(200, y_location +15)
+        t.write("$"+str(price), font=("Arial", 15), align="center")
+    
+    print("Welcome to the Museum of Titus!")
+    print("We heard you were interested in a visit.")
+    print("We need to ask you some questions before buying a ticket.")
+    print(" ")
+    first=input("What is your first name? ")
+    last=input("What is your last name? ")
+    name=first+" "+last
+    print("Thank you, "+first+"!")
+    while True:
+        age=int(input("What is your age? "))
+        if age<=0:
+            print("I don't think sperm cells can see, try putting in your real age.")
+        elif age>=115:
+            print("Wow you're really old, try putting in your real age.")
+        else:
+            break
+    if age<=3:
+        age="3 and under"
+    elif age<=17:
+        age="Kid"
+    else:
+        age="Adult"
+    while True:
+        if age=="Kid" or age=="Adult":
+            student=input("Are you a high school, or college student? ")
+            if student=="yes" or student=="Yes" or student=="YES" or student=="y" or student=="Y":
+                age=="Kid"
+                student="yes"
+                print("You qualify for the student discount!")
+                break
+            elif student=="no" or student=="No" or student=="NO" or student=="n" or student=="N":
+                student="no"
+                break
+            else:
+                print("I didn't get that, please try typing 'yes' or 'no'.")
+    while True:
+        day=input("What day do you plan to visit the museum? ")
+        if day=="sunday" or day=="su" or day=="Sunday" or day=="sun":
+            day="Sunday"
+            break
+        elif day=="monday" or day=="mo" or day=="Monday" or day=="mon":
+            day="Monday"
+            break
+        elif day=="tuesday" or day=="tu" or day=="Tuesday" or day=="tue" or day=="tues":
+            day="Tuesday"
+            break
+        elif day=="wednesday" or day=="we" or day=="Wednesday" or day=="wed":
+            day="Wednesday"
+            break
+        elif day=="thursday" or day=="th" or day=="Thursday" or day=="thu" or day=="thurs":
+            day="Thursday"
+            break
+        elif day=="friday" or day=="fr" or day=="Friday" or day=="fri":
+            day="Friday"
+            break
+        elif day=="saturday" or day=="sa" or day=="Saturday" or day=="sat":
+            day="Saturday"
+            break
+        else:
+            print("I didn't get that, try typing 'Monday' or 'Friday'.")
+    print(" ")
+    print("We are getting ready for your visit on "+day+".")
+    coupon=input("Do you have a coupon code for your visit? ")
+    if coupon=="FREEFRIDAY" and age=="Adult" and student=="no":
+        print("I'm sorry, that coupon is only valid or children and students.")
+    elif coupon=="FREEFRIDAY" and age=="Kid" and day=="Friday":
+        print("Coupon FREEFRIDAY succesful! You are eligible for a free visit.")
+        coupon=1
+    elif coupon=="FREEFRIDAY" and age=="Kid" and day!="Friday":
+        print("I'm sorry, that coupon is only valid on Fridays.")
+        
+    elif coupon=="SUNDAY10" and age=="Adult" and student=="no":
+        print("I'm sorry, that coupon is only valid or children and students.")
+    elif coupon=="SUNDAY10" and age=="Kid" and day=="Sunday":
+        print("Coupon SUNDAY10 succesful! You are eligible for $10 off.")
+        coupon=2
+    elif coupon=="SUNDAY10" and age=="Kid" and day!="Sunday":
+        print("I'm sorry, that coupon is only valid on Sunday.")
+    else:
+        print("Coupon code invalid.")
+        
+    if age=="Kid" and student=="yes":
+        age="Student"
+
+    print(" ")
+    print(name+", you are eligible for a "+age+" Ticket on "+day+".")
+    if age=="3 and under":
+        price=0
+    elif age=="Kid" or age=="Student":
+        price=50
+        if day=="Saturday" or day=="Sunday":
+            price=100
+        if coupon==1:
+            price=0
+        elif coupon==2:
+            price=price-10
+    elif age=="Adult":
+        price=100
+    print("Your total is $"+str(price)+".")
+    print("Please proceed to the payment screen to pay and claim your ticket.")
+    draw_ticket(name, price, day, 0)
+    
+#main
+#draw_ticket("Titus Lau", 5, "Friday", 0)
+ticket_quiz()
